2c0fa6e52e219fa0e59c5a448c95153a58192958
Midorikami <150435939+KuroKami2023@users.noreply.github.com>
2024-04-08 10:16:36 +1000

    UPDATE: 04-08-2024 AM

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,37 @@
 function toggleSize(element) {
     var selectedFiles = document.querySelectorAll('.selected-file');
-    if (selectedFiles.length > 0) {
+    var selectedFilesSplit = document.querySelectorAll('.selected-file-split');
+    var selectedFilesImg = document.querySelectorAll('.selected-file-img');
+    if (selectedFiles.length > 0 ||
+        selectedFilesSplit.length > 0 ||
+        selectedFilesImg.length > 0) {
         return;
     }
 
     var target = event.target;
     if (target.id === "file-select-btn" || 
     target.id === "file-input" || 
+    target.id === "file-select-splitbtn" || 
+    target.id === "file-input-split" || 
+    target.id === "file-select-imgbtn" || 
+    target.id === "file-input-img" || 
     target.classList.contains("remove-file-btn") ||
     target.classList.contains("selected-file-name") ||
     target.classList.contains("selected-file") ||
     target.classList.contains("file-name-container") ||
     target.classList.contains("remove-file-btn") ||
     target.classList.contains("fa-xmark") ||
-    target.classList.contains("fileicon")) {
+    target.classList.contains("fileicon") ||
+    target.classList.contains("remove-file-btn-split") ||
+    target.classList.contains("selected-file-name-split") ||
+    target.classList.contains("selected-file-split") ||
+    target.classList.contains("file-name-container-split") ||
+    target.classList.contains("remove-file-btn-split") ||
+    target.classList.contains("remove-file-btn-img") ||
+    target.classList.contains("selected-file-name-img") ||
+    target.classList.contains("selected-file-img") ||
+    target.classList.contains("file-name-container-img") ||
+    target.classList.contains("remove-file-btn-img")) {
         return;
     }
 
@@ -47,12 +65,12 @@ function toggleSize(element) {
     }
 }
 
+//<==================================================MERGE=====================================================================>
 
 var btnselect = document.getElementById('file-select-btn');
 var btnmerge = document.getElementById('mergebtn');
 
 document.getElementById("file-select-btn").addEventListener("click", function() {
-    console.log("ehhhh");
     document.getElementById("file-input").click();
     btnselect.style.display = 'none';
 });
@@ -61,31 +79,52 @@ document.getElementById("file-input").addEventListener("change", function(event)
     if (event.target.files.length === 0) {
         document.getElementById("file-select-btn").style.display = "block";
     }
-    displaySelectedFiles(event.target.files);
+    displaySelectedFiles(event.target.files, 'merge');
 });
    
 
-function displaySelectedFiles(files) {
-    var selectedFileContainer = document.getElementById("selected-file-info");
+function displaySelectedFiles(files, mode) {
+    var selectedFileContainer = document.getElementById(
+        mode === 'merge' ? "selected-file-info" : 
+        mode === 'split' ? "selected-file-info-split" :
+        "selected-file-info-img"
+    );
     selectedFileContainer.innerHTML = '';
     if (files.length > 0) {
         for (var i = 0; i < files.length; i++) {
             var fileEntry = document.createElement('div');
-            fileEntry.classList.add('selected-file');
+            fileEntry.classList.add(
+                mode === 'merge' ? 'selected-file' : 
+                mode === 'split' ? 'selected-file-split' : 
+                'selected-file-img');
             
             var fileNameContainer = document.createElement('span');
-            fileNameContainer.classList.add('file-name-container');
+            fileNameContainer.classList.add(
+                mode === 'merge' ? 'file-name-container' : 
+                mode === 'split' ? 'file-name-container-split' :
+                'file-name-container-img');
             
             var fileName = document.createElement('span');
-            fileName.classList.add('selected-file-name');
+            fileName.classList.add(
+                mode === 'merge' ? 'selected-file-name' : 
+                mode === 'split' ? 'selected-file-name-split' :
+                'selected-file-name-img');
             fileName.textContent = files[i].name;
             
             var iconSpan = document.createElement('span');
-            iconSpan.innerHTML = '<i class="fa-solid fa-file-lines fileicon"></i>';
+            iconSpan.classList.add('fileicon');
+            if (mode === 'img') {
+                iconSpan.innerHTML = '<i class="fa-solid fa-image"></i>';
+            } else {
+                iconSpan.innerHTML = '<i class="fa-solid fa-file-lines"></i> ';
+            }
             
             var removeBtn = document.createElement('button');
             removeBtn.innerHTML = 'x';
-            removeBtn.classList.add('remove-file-btn');
+            removeBtn.classList.add(
+                mode === 'merge' ? 'remove-file-btn' : 
+                mode === 'split' ? 'remove-file-btn-split' :
+                'remove-file-btn-img');
 
             fileNameContainer.appendChild(iconSpan);
             fileNameContainer.appendChild(fileName);
@@ -96,14 +135,22 @@ function displaySelectedFiles(files) {
             selectedFileContainer.appendChild(fileEntry);
         }
         selectedFileContainer.style.display = "block";
-        document.getElementById("mergebtn").style.display = "block"; 
+        document.getElementById(
+            mode === 'merge' ? "mergebtn" : 
+            mode === 'split' ? "splitbtn" :
+            "imgbtn").style.display = "block"; 
     } else {
         selectedFileContainer.style.display = "none"; 
-        document.getElementById("mergebtn").style.display = "none"; 
-        document.getElementById("file-select-btn").style.display = "block"; 
+        document.getElementById(
+            mode === 'merge' ? "mergebtn" : 
+            mode === 'merge' ? "splitbtn" :
+            "imgbtn").style.display = "none"; 
+        document.getElementById(
+            mode === 'merge' ? "file-select-btn" : 
+            mode === 'merge' ? "file-select-splitbtn" :
+            "file-select-imgbtn").style.display = "block"; 
     }
 }
-
 
 document.getElementById("selected-file-info").addEventListener("click", function(event) {
     if (event.target.classList.contains("remove-file-btn")) {
@@ -117,3 +164,71 @@ document.getElementById("selected-file-info").addEventListener("click", function
         }
     }
 });
+
+//<============================================================================================================================>
+
+//<==================================================SPLIT=====================================================================>
+
+var splitbtnselect = document.getElementById('file-select-splitbtn');
+var btnsplit = document.getElementById('splitbtn');
+
+document.getElementById("file-select-splitbtn").addEventListener("click", function() {
+    document.getElementById("file-input-split").click();
+    splitbtnselect.style.display = 'none';
+});
+
+document.getElementById("file-input-split").addEventListener("change", function(event) {
+    if (event.target.files.length === 0) {
+        document.getElementById("file-select-splitbtn").style.display = "block";
+    }
+    displaySelectedFiles(event.target.files, 'split');
+});
+   
+
+document.getElementById("selected-file-info-split").addEventListener("click", function(event) {
+    if (event.target.classList.contains("remove-file-btn-split")) {
+        var fileEntrySplit = event.target.parentElement;
+        fileEntrySplit.remove();
+        var remainingFilesSplit = document.querySelectorAll('.selected-file-split');
+        if (remainingFilesSplit.length === 0) {
+            document.getElementById("selected-file-info-split").style.display = "none";
+            document.getElementById("splitbtn").style.display = "none";
+            document.getElementById("file-select-splitbtn").style.display = "block";
+        }
+    }
+});
+
+//<==========================================================================================================================>
+
+//<==================================================IMG=====================================================================>
+
+var imgbtnselect = document.getElementById('file-select-imgbtn');
+var btnimg = document.getElementById('imgbtn');
+
+document.getElementById("file-select-imgbtn").addEventListener("click", function() {
+    document.getElementById("file-input-img").click();
+    imgbtnselect.style.display = 'none';
+});
+
+document.getElementById("file-input-img").addEventListener("change", function(event) {
+    if (event.target.files.length === 0) {
+        document.getElementById("file-select-imgbtn").style.display = "block";
+    }
+    displaySelectedFiles(event.target.files, 'img');
+});
+   
+
+document.getElementById("selected-file-info-img").addEventListener("click", function(event) {
+    if (event.target.classList.contains("remove-file-btn-img")) {
+        var fileEntryImg = event.target.parentElement;
+        fileEntryImg.remove();
+        var remainingFilesImg = document.querySelectorAll('.selected-file-img');
+        if (remainingFilesImg.length === 0) {
+            document.getElementById("selected-file-info-img").style.display = "none";
+            document.getElementById("imgbtn").style.display = "none";
+            document.getElementById("file-select-imgbtn").style.display = "block";
+        }
+    }
+});
+
+//<============================================================================================================================>
