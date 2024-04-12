cfe8bda7227a44a9d9c60c323eab307a79e30297
Midorikami <150435939+KuroKami2023@users.noreply.github.com>
2024-04-12 10:34:47 +1000

    excel.js

--- JS/excel.js
+++ JS/excel.js
@@ -15,6 +15,7 @@ async function convertToExcelWithOCR(inputPath) {
             IncludeFormatting: 'false',
             EnableOCR: 'true',
             SingleSheet: 'true',
+            StoreFile: 'false',
         });
 
         await result.saveFiles(outputPath);
